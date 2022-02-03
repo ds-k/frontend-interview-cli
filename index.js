7be1400d66d7a7561a78138a1ec279378aeb9d07
daeseongkim05 <daeseongkim05@gmail.com>
2022-02-03 20:50:25 +0900

    add sample question

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
 import { createRequire } from "module";
+import { link } from "fs";
+import open from "open";
 const require = createRequire(import.meta.url);
 const quizList = require("./quizList.json");
 
@@ -17,22 +19,17 @@ const sleep = (ms = 1500) => new Promise((r) => setTimeout(r, ms));
 
 const onboarding = async () => {
   const rainbowTitle = chalkAnimation.rainbow(
-    `프론트엔드 개발자 면접을 위한 질문 모음\n`
+    `\n프론트엔드 개발자 면접을 위한 질문 모음 by.daeseongkim05\n`
   );
 
   await sleep();
   rainbowTitle.stop();
-
-  console.log(
-    `${chalk.blue(
-      "Github repo : https://github.com/daeseongkim05/frontend-interview-cli"
-    )}\n`
-  );
   await qList();
 };
 
 // ! 목록
 async function qList() {
+  console.clear();
   curCategory = "";
   const lists = await inquirer.prompt({
     name: "category",
@@ -47,19 +44,14 @@ async function qList() {
 
 // ! 문제
 async function quiz(category) {
+  console.clear();
   const quizCategory = quizList[category];
   curNum = Math.floor(Math.random() * quizCategory.length);
 
-  console.log(`
-  
-${quizCategory[curNum].Q}`);
+  console.log(`${quizCategory[curNum].Q}`);
 
-  const radarTitle = chalkAnimation.rainbow(
-    `
-
-충분히 생각해보고 정답을 확인해보세요.(아무 키나 눌러주세요.)
-
-      `
+  chalkAnimation.karaoke(
+    `\n\n충분히 생각해보고 정답을 확인해보세요.(아무 키나 눌러주세요.)\n\n`
   );
 
   await inquirer.prompt({
@@ -74,22 +66,21 @@ ${quizCategory[curNum].Q}`);
 async function handleQuizAnswer(options) {
   if (options === "다음 문제") {
     quiz(curCategory);
+  } else if (options === "깃허브 방문하기") {
+    open("https://github.com/daeseongkim05/frontend-interview-cli");
+    qList();
   } else {
     qList();
   }
 }
 
 async function showAnswer(category, num) {
-  console.log(`
-  
-  ${quizList[category][num].A}
-    
-    `);
+  console.log(`\n\n${quizList[category][num].A}\n\n`);
   const quiz = await inquirer.prompt({
     name: "options",
     message: "더 푸시겠습니까?",
     type: "list",
-    choices: ["다음 문제", "목록으로 가기"],
+    choices: ["다음 문제", "목록으로 가기", "깃허브 방문하기"],
   });
   return handleQuizAnswer(quiz.options);
 }
